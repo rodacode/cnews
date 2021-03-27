ed7a10d70bcc07e5d24ebe0f448dfbf6fdda7684
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-03-27 14:30:39 +0100

    added twiiter link and trending fallback

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,23 +1,30 @@
+import Link from 'next/link'
 import { Flex, Box } from '@chakra-ui/react'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const SocialIcons = () => (
-    <>
-        <Flex justifyContent="center" flexDirection="row" maxW={'15vv'}>
-        </Flex>
-        <Flex justifyContent="center">
-            <Box m="4"><FontAwesomeIcon icon={faInstagram} /></Box>
-            <Box m="4"><FontAwesomeIcon icon={faYoutube} /></Box>
-            <Box m="4"><FontAwesomeIcon icon={faTwitter} /></Box>
-            
-        </Flex>
-    </>
+const SocialIcons = () => {
+    return (
+        <>
+            <Flex justifyContent="center" flexDirection="row" maxW={'15vv'}>
+            </Flex>
+            <Flex justifyContent="center">
+                <Box m="4">
+                    <FontAwesomeIcon icon={faInstagram} />
 
-)
+                </Box>
 
+                <Box m="4"><FontAwesomeIcon icon={faYoutube} /></Box>
+                <Link href="https://twitter.com/thecryptoroad?s=09"><a target="_blank">
+                    <Box m="4"><FontAwesomeIcon icon={faTwitter} /></Box>
+                </a>
+                </Link>
+            </Flex>
+        </>
+    )
+}
 
 export default SocialIcons
 
--- src/components/TrendingCoins.js
+++ src/components/TrendingCoins.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Flex, Box, Heading, Image, Skeleton } from '@chakra-ui/react';
+import { Flex, Box, Heading, Image } from '@chakra-ui/react';
 import {
     Table,
     Thead,
@@ -30,33 +30,36 @@ const TrendingCoins = () => {
     return (
         <Box boxShadow="dark-lg" color="#000" bg="#f6f6f6" p="10" m="4" borderRadius="50">
             <Flex flexDirection='column' flexWrap="wrap">
-                <Skeleton isLoaded={!isLoading}>
-                    <Heading textAlign="center">Trending coins on Coingecko</Heading>
-                    <Table variant="simple">
-                        <Thead>
-                            <Tr>
-                                <Th>Token</Th>
-                                <Th>Name</Th>
-                                <Th>Ranking</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {coins && coins.map((coin) => {
-                                return (
-                                    <React.Fragment key={coin.id}>
-                                        <Tr>
-                                            <Td d="flex" flexDirection="row">
-                                                <Image mr="2" src={coin.item.thumb} boxSize="20px"
-                                                />{coin.item.symbol.toUpperCase()}</Td>
-                                            <Td>{coin.item.name.toUpperCase()}</Td>
-                                            <Td>#{coin.item.market_cap_rank}</Td>
-                                        </Tr>
-                                    </React.Fragment>
-                                )
-                            })}
-                        </Tbody>
-                    </Table>
-                </Skeleton>
+
+                <Heading textAlign="center">Trending coins on Coingecko</Heading>
+                {isLoading ?
+                    <>
+                        <Table variant="simple">
+                            <Thead>
+                                <Tr>
+                                    <Th>Token</Th>
+                                    <Th>Name</Th>
+                                    <Th>Ranking</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                {coins && coins.map((coin) => {
+                                    return (
+                                        <React.Fragment key={coin.id}>
+                                            <Tr>
+                                                <Td d="flex" flexDirection="row">
+                                                    <Image mr="2" src={coin.item.thumb} boxSize="20px"
+                                                    />{coin.item.symbol.toUpperCase()}</Td>
+                                                <Td>{coin.item.name.toUpperCase()}</Td>
+                                                <Td>#{coin.item.market_cap_rank}</Td>
+                                            </Tr>
+                                        </React.Fragment>
+                                    )
+                                })}
+                            </Tbody>
+                        </Table>
+                    </> : <Image src="./images/trending_coins_fallback.png" />
+                }
             </Flex>
         </Box>
     );
