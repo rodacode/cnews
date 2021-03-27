540890aa2f1203bca197d6c043db44bb91a9c181
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-03-27 21:04:40 +0100

    fix sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,38 +1,52 @@
-const fs = require('fs');
-const globby = require('globby');
+const fs = require("fs");
+const globby = require("globby");
+const prettier = require("prettier");
 
-const generateSitemap = async () => {
-  // Fetch all routes based on patterns
-  // Your folder structure might be different so change bellow to match your needs
+const getDate = new Date().toISOString();
+
+const YOUR_AWESOME_DOMAIN = "https://www.thecryptoroad.com";
+
+const formatted = sitemap => prettier.format(sitemap, { parser: "html" });
+
+(async () => {
   const pages = await globby([
-    'pages/**/*.{ts,tsx,mdx}', // All routes inside /pages
-    '_content/**/*.mdx', // All MDX files inside my /_content
-    '!pages/**/[*.{ts,tsx}', // Ignore my dynamic route index Example /pages/blog/[slug].tsx
-    '!pages/_*.{ts,tsx}', // Ignore next.js files
-    '!pages/api', // Ignore API routes
-    '!pages/admin.tsx' // Ignore pages not meant to be indexed
+    // include
+    "../pages/**/*.tsx",
+    "../pages/*.tsx",
+    // exclude
+    "!../pages/_*.tsx"
   ]);
 
-  const urlSet = pages
-    .map((page) => {
-      // Remove none route related parts of filename.
-      const path = page
-        .replace('pages', '')
-        .replace('_content', '')
-        .replace(/(.tsx|.ts)/, '')
-        .replace('.mdx', '');
-      // Remove the word index from route
-      const route = path === '/index' ? '/' : path;
-      // Build url portion of sitemap.xml
-      return `<url><loc>https://thecryptoroad.com${route}</loc></url>`;
-    })
-    .join('');
+  const pagesSitemap = `
+    ${pages
+      .map(page => {
+        const path = page
+          .replace("../pages/", "")
+          .replace(".tsx", "")
+          .replace(/\/index/g, "");
+        const routePath = path === "index" ? "" : path;
+        return `
+          <url>
+            <loc>${YOUR_AWESOME_DOMAIN}/${routePath}</loc>
+            <lastmod>${getDate}</lastmod>
+          </url>
+        `;
+      })
+      .join("")}
+  `;
 
-  // Add urlSet to entire sitemap string
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlSet}</urlset>`;
+  const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${pagesSitemap}
+    </urlset>
+  `;
 
-  // Create sitemap file
-  fs.writeFileSync('public/sitemap.xml', sitemap);
-};
+  const formattedSitemap = [formatted(generatedSitemap)];
 
-module.exports = generateSitemap;
+  fs.writeFileSync("../public/sitemap.xml", formattedSitemap, "utf8");
+})();
