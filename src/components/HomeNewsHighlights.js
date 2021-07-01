b5f9e0e47c4d946ba9bb8748c057fb6f9a6b3631
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-07-01 19:42:37 +0200

    news

--- src/components/HomeNewsHighlights.js
+++ src/components/HomeNewsHighlights.js
@@ -4,17 +4,16 @@ const HomeNewsHighlights = () => {
   return (
     <>
       <NewsCard
-        title="Binance Launches Zero-Commission, Tradable Stock Tokens"
-        description="Binance announces the official launch of its zero-commission, tradable stock tokens, allowing the users to trade fractional stocks. Stock tokens are denominated, settled, and collateralized in BUSD.
-        The first Binance Stock Token to be listed is Tesla Inc. (TSLA). Trading for the TSLA/BUSD pair is scheduled to open at 2021-04-12 1:35 PM (UTC). Users will be able to trade fractional Tesla stock on the Binance website."
-        imageUrl="./images/teslaNews.jpeg"
-        url="https://www.binance.com/en/support/articles/2c64611658c645a59e05ef12f02c22ab"
+        title="Kusama’s parachain auctions"
+        description="Eleven projects joined the race when Kusama parachain auctions started on June 15. A few more got in during the latter auctions of the first batch. With impressive gains to the tune of 900,000 KSM (approximately $180 million, at the time of writing), are Kusama auctions still fair and democratic, or is everything already decided by whales?"
+        imageUrl="./images/kusama.png"
+        url="https://cointelegraph.com/news/how-much-intrigue-is-behind-kusama-s-parachain-auctions"
       />
       <NewsCard
-        title="Coinbase made more in Q1 2021 than all of 2020"
-        description="The release showed Coinbase made over $1.8 billion (revenue) in Q1 of 2021—an amount greater than the $1.1 billion figure for all of 2020 and nearly ten times the $190 million revenue in the first quarter of last year."
-        imageUrl="./images/coinbase_q1_2021.png"
-        url="#"
+        title="$22B hedge fund Point72 reportedly searching for a 'head of crypto'"
+        description="New York billionaire Steven Cohen’s hedge fund Point72 Asset Management, is reportedly searching for a “head of crypto."
+        imageUrl="./images/cointelegraph.jpeg"
+        url="https://cointelegraph.com/news/22b-hedge-fund-point72-reportedly-searching-for-a-head-of-crypto"
       />
     </>
   );
