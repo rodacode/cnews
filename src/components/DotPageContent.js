92618168eadfbec5ca22e35137a2e3c736a1312a
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-09-30 22:21:14 +0200

    dot page

--- src/components/DotPageContent.js
+++ src/components/DotPageContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header'
 import { Container } from './Container'
-import { Box, Text, Image } from '@chakra-ui/react';
+import { Box, Text, Image, ListItem, UnorderedList, Heading } from '@chakra-ui/react';
 
 import HomeDOTData from './HomeDOTData'
 import DOTTweetsContainer from './DOTTweetsContainer'
@@ -14,18 +14,32 @@ const DotPageContent = ({ element }) => {
             <Header />
 
             <Box minW="100vw" ref={element}>
-                <Box width={"90%"} mr="auto" ml="auto" d="flex" flexDirection="row" flexWrap="wrap" justifyContent="center">
-                    <Box p="2" mt="8" width={["100%","50%"]}>
+                <Box width={"90%"} mr="auto" ml="auto" d="flex" flexDirection="column" flexWrap="wrap" justifyContent="center">
+                <Heading textAlign="center" mt="12" mb="12">POLKADOT</Heading>
+                <Box width={"90%"} mr="auto" ml="auto" d="flex" flexDirection="row" flexWrap="wrap" justifyContent="center" alignItems="center">
+
+                    <Box p="2" mt="8" width={["100%", "50%"]}>
                         <Text fontSize="16px">
-                        Polkadot will enable a completely decentralized web where users are in control.
-                        Polkadot is built to connect private and consortium chains, public and permissionless networks, oracles, and future technologies that are yet to be created. Polkadot facilitates an internet where independent blockchains can exchange information and transactions in a trustless way via the Polkadot relay chain.
-                        Polkadot makes it easier than ever to create and connect decentralized applications, services, and institutions. By empowering innovators to build better solutions, we seek to free society from its reliance on a broken web where its large institutions can’t violate our trust.
-                     </Text>
+                            Polkadot is a layer 0 blockchain created by Dr. Gavin Wood, co-founder of Ethereum, creator of Solidity , the smart contracts language used in ETH.
+                        </Text>
+                        <Text fontSize="24px" mt="12" mb="4">POLKADOT KEY FEATURES</Text>
+                        <UnorderedList>
+                            <ListItem>Heterogeneity</ListItem>
+                            <ListItem>Scalability</ListItem>
+                            <ListItem>Interoperability</ListItem>
+                            <ListItem>Shared security</ListItem>
+                            <ListItem>On-chain governance</ListItem>
+                            <ListItem>Forkless upgrades</ListItem>
+                            <ListItem>Extra key security</ListItem>
+                            <ListItem>Novel economics and game theory</ListItem>
+                        </UnorderedList>
                     </Box>
-                    <Box p="10" d="flex" justifyContent="center" width={["100%","50%"]}>
+                    <Box p="10" d="flex" justifyContent="center" width={["100%", "50%"]}>
                         <Image boxSize="280px" borderRadius="5px" src={'./images/dot.png'} alt="polkadot logo" />
                     </Box>
                 </Box>
+                </Box>
+
 
                 <HomeDOTData />
                 <DOTTweetsContainer />
