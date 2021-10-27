8529af8ed7bf583514fbcd6bb25a7117f731a898
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-10-27 11:58:14 +0200

    fix import

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import { Flex, Heading } from '@chakra-ui/react'
-import TopTenCoins from './TopTenCoins/TopTenCoins'
+import TopTenCoins from './topTenCoins/TopTenCoins'
 import AdBanner from './AdBanner'
 
 export const Sidebar = () => (
