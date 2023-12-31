15f74532e1b736b3697fdcfc1c64b143ef675778
Nico <nicolas.hardmeier@yahoo.es>
2023-12-31 17:32:44 +0100

    dd

--- src/components/NavBarContent.js
+++ src/components/NavBarContent.js
@@ -23,54 +23,7 @@ import {
 } from "@chakra-ui/icons";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 import SocialIcons from "./SocialIcons";
-
-const NAV_ITEMS = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Bitcoin",
-    href: "/bitcoin",
-  },
-  {
-    label: "Solana",
-    href: "/solana",
-  },
-  {
-    label: "Ethereum",
-    href: "/ethereum",
-  },
-
-  {
-    label: "Polkadot",
-    href: "/polkadot",
-  },
-  {
-    label: "Kusama",
-    href: "/kusama",
-  },
-  {
-    label: "Moonriver",
-    href: "/moonriver",
-  },
-  {
-    label: "Altcoins",
-    href: "/altcoins",
-  },
-  {
-    label: "DEFI",
-    href: "/defi",
-  },
-  {
-    label: "Crypto Wiki",
-    href: "/cryptoWiki",
-  },
-  {
-    label: "On Twitter",
-    href: "/twitter",
-  },
-];
+import { NAV_ITEMS } from "../constants/constans";
 
 export default function NavBarContent() {
   const { isOpen, onToggle } = useDisclosure();
---
+++ src/constants/constans.js
@@ -0,0 +1,47 @@
+export const NAV_ITEMS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Bitcoin",
+    href: "/bitcoin",
+  },
+  {
+    label: "Solana",
+    href: "/solana",
+  },
+  {
+    label: "Ethereum",
+    href: "/ethereum",
+  },
+
+  {
+    label: "Polkadot",
+    href: "/polkadot",
+  },
+  {
+    label: "Kusama",
+    href: "/kusama",
+  },
+  {
+    label: "Moonriver",
+    href: "/moonriver",
+  },
+  {
+    label: "Altcoins",
+    href: "/altcoins",
+  },
+  {
+    label: "DEFI",
+    href: "/defi",
+  },
+  {
+    label: "Crypto Wiki",
+    href: "/cryptoWiki",
+  },
+  {
+    label: "On Twitter",
+    href: "/twitter",
+  },
+];
