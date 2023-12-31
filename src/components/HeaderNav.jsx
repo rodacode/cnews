ac8a6e706670ca6459cc2bd7806a7d790029cc11
Nico <nicolas.hardmeier@yahoo.es>
2023-12-31 17:53:07 +0100

    dd

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,46 +1,44 @@
-import { Box, HStack, Link, useColorModeValue } from '@chakra-ui/react'
+import { Box, HStack, Link, useColorModeValue } from "@chakra-ui/react";
 
 const Links = [
-    { title: 'HOME', link: '/' },
-    { title: 'BITCOIN', link: '/bitcoin' },
-    { title: 'CRYPTO WIKI', link: '/cryptoWiki' },
-    { title: 'DEFI', link: '/defi' },
-    { title: 'POLKADOT', link: '/polkadot' },
-    { title: 'KUSAMA', link: '/kusama' },
-    { title: 'MOONRIVER', link: '/moonriver' },
-    { title: 'ALTCOINS', link: '/altcoins' },
-    { title: 'ON TWITTER', link: '/twitter' }
+  { title: "HOME", link: "/" },
+  { title: "BITCOIN", link: "/bitcoin" },
+  { title: "ETHEREUM", link: "/ethereum" },
+  { title: "POLKADOT", link: "/polkadot" },
+  { title: "SOLANA", link: "/solana" },
+  { title: "KUSAMA", link: "/kusama" },
+  { title: "MOONRIVER", link: "/moonriver" },
+  { title: "CRYPTO WIKI", link: "/cryptoWiki" },
+  { title: "DEFI", link: "/defi" },
+  { title: "ALTCOINS", link: "/altcoins" },
+  { title: "ON TWITTER", link: "/twitter" },
 ];
 
 const NavLink = ({ title, link }) => (
-    <Link
-        px={2}
-        py={1}
-        rounded={'md'}
-        _hover={{
-            textDecoration: 'none',
-            bg: useColorModeValue('gray.200', 'gray.700'),
-        }}
-        href={link}>
-        {title}
-    </Link>
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={link}
+  >
+    {title}
+  </Link>
 );
 
 const HeaderNav = () => (
-    <Box>
-        <HStack spacing={8} alignItems={'center'}>
-            <HStack
-                as={'nav'}
-                spacing={4}
-                display={{ base: 'none', md: 'flex' }}>
-                {Links.map((item) => (
-                    <NavLink key={item.title} title={item.title} link={item.link} />
-                ))}
-            </HStack>
-        </HStack>
-    </Box >
-)
+  <Box>
+    <HStack spacing={8} alignItems={"center"}>
+      <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+        {Links.map((item) => (
+          <NavLink key={item.title} title={item.title} link={item.link} />
+        ))}
+      </HStack>
+    </HStack>
+  </Box>
+);
 
-
-export default HeaderNav
-
+export default HeaderNav;
